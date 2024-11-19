a9ef9e0c7a3b05f9e4f0adeec2a6bfb062927a83
Max Starceski <123509417+mstarceski@users.noreply.github.com>
2024-11-19 14:09:16 -0700

    Update Application.py

--- Application.py
+++ Application.py
@@ -3,19 +3,6 @@ import pandas as pd
 import random
 import time
 
-#stolen function
-def response_generator():
-    response = random.choice(
-        [
-            "Ramiro Jordan: The Data",
-            "Ramiro Jordan: (smiles deviously)",
-            "Ramiro Jordan: Alright more Packbacks, due today.",
-        ]
-    )
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
-
 
 
 # Title of the app
@@ -33,47 +20,13 @@ try:
     with open('example.txt', 'r') as file:
         content = file.read()
     st.write(content)
-
-    
-    #sample code I stole from the docs:
-    # Initialize chat history
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
-    
-    # Display chat messages from history on app rerun
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
-
-    # React to user input
-    if prompt := st.chat_input("What is up?"):
-        # Display user message in chat message container
-        st.chat_message("user").markdown(prompt)
-        # Add user message to chat history
-        st.session_state.messages.append({"role": "user", "content": prompt})
-    
-        with st.chat_message("assistant"):
-            response = st.write_stream(response_generator())
-         # Add assistant response to chat history
-        st.session_state.messages.append({"role": "assistant", "content": response})
-            
-        with open("example.txt", "w") as file:
-            # Write the string to the file
-            file.write(prompt)
     
     # Dropdowns for X and Y axes selection
     columns = df.columns.tolist()
     x_axis = st.selectbox('Select column for X-axis', columns)
     y_axis = st.selectbox('Select column for Y-axis', columns)
     
-    # Choose chart type: Bar Chart or Line Chart
-    chart_type = st.selectbox('Select chart type', ['Bar Chart', 'Line Chart'])
-    
-    # Display chart based on user selections
-    if chart_type == 'Bar Chart':
-        st.bar_chart(df[[x_axis, y_axis]].set_index(x_axis))
-    elif chart_type == 'Line Chart':
-        st.line_chart(df[[x_axis, y_axis]].set_index(x_axis))
+    st.line_chart(df[[x_axis, y_axis]].set_index(x_axis))
 
 
 
