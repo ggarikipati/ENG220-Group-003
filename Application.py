4f265bff428e5865e35cd8cbe4105e8808d4d013
Max Starceski <123509417+mstarceski@users.noreply.github.com>
2024-11-29 14:36:25 -0700

    Update Application.py

--- Application.py
+++ Application.py
@@ -27,7 +27,7 @@ try:
 
     # Compute moving average
     if moving_avg_column:
-       df['Moving Average'] = df[moving_avg_column].rolling(window=window_size).mean()
+        df['Moving Average'] = df[moving_avg_column].rolling(window=window_size).mean()
         st.write(f"Moving Average for {moving_avg_column} (Window Size: {window_size}):")
         st.line_chart(df[['Moving Average', moving_avg_column]])
     
