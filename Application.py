839cef793b2d321568461f20f0eab9d470fb2dac
Max Starceski <123509417+mstarceski@users.noreply.github.com>
2024-11-29 21:48:34 -0700

    Update Application.py

--- Application.py
+++ Application.py
@@ -25,7 +25,8 @@ try:
         window_size = 1000
         df['Moving Average'] = df[moving_avg_column].rolling(window_size).sum()/window_size
         st.write(f"Moving Average for {moving_avg_column} (Window Size: {window_size}):")
-        st.line_chart(df[['Time (Days)', moving_avg_column,'Moving Average']].set_index('Time (Days)'), color=["#2491D9","#D51616"])
+        #st.line_chart(df[['Time (Days)', moving_avg_column,'Moving Average']].set_index('Time (Days)'), color=["#2491D9","#D51616"])
+        st.line_chart(df[['Time (Days)', 'Moving Average']].set_index('Time (Days)'), color=["#2491D9"])
 
 
 except FileNotFoundError:
